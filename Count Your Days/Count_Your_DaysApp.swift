85dd97c04f5ada96c6c61e518e28980ce56cf354
Alex <132796685+aruss03@users.noreply.github.com>
2025-04-17 22:47:05 -0400

    Initial Commit

---
+++ Count Your Days/Count_Your_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_Your_DaysApp.swift
+//  Count Your Days
+//
+//  Created by Alex Russ on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Count_Your_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Count Your DaysTests/Count_Your_DaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  Count_Your_DaysTests.swift
+//  Count Your DaysTests
+//
+//  Created by Alex Russ on 4/17/25.
+//
+
+import Testing
+@testable import Count_Your_Days
+
+struct Count_Your_DaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
